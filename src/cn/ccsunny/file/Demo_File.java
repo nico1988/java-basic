534c41b8fda99a9415df5da24e68424c62e4d3d9
nico1988 <410827024@qq.com>
2019-02-17 23:30:37 +0800

    file

---
+++ src/cn/ccsunny/file/Demo_File.java
@@ -0,0 +1,7 @@
+package src.cn.ccsunny.file;
+
+public class Demo_File {
+    public static void main(String[] args){
+        System.out.println("1");
+    }
+}
--- src/cn/ccsunny/math/Demo_Math.java
+++ src/cn/ccsunny/math/Demo_Math.java
@@ -1,5 +1,7 @@
 package cn.ccsunny.math;
 
 public class Demo_Math {
-
+    public static void main(String[] args){
+        System.out.println(111);
+    }
 }
